f1836137c0044cef5c5bb777f33af9000283d64c
Andrew Waller <andrew.waller@saplingdata.com>
2022-07-14 20:58:33 -0500

    Update pageName and navmode

--- loki.config.mjs
+++ loki.config.mjs
@@ -8,14 +8,14 @@ export default {
   // The app builder associated with your app model
   appBuilderName: 'examples-modeler',
   // The page in Loki's App Builder that you plan to deploy to
-  pageName: 'helloWorldSvelte',
+  pageName: 'svelteBase',
   // The subdomain of your cloud's url
   cloudPrefix: 'dev',
   // The name of your cloud environment
   cloudName: 'saplingdata',
   // The root of your app urn; typically the same as cloudName, but can vary for older apps
   appRoot: 'loki',
-  navMode: 'navbar',
+  navMode: 'sidebar',
   // Is your app a Sapling internal app or a client app?
   internal: true,
 };
