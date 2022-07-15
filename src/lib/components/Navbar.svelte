235be7771a45fe0e97c7edb39db93d839352f08d
Andrew Waller <andrew.waller@saplingdata.com>
2022-07-14 20:56:40 -0500

    Update navbar

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -16,11 +16,10 @@ import { Icon } from '@steeze-ui/svelte-icon';
 import type { IconThemeSource } from '@steeze-ui/svelte-icon';
 import {Briefcase, ChevronDown, Logout, User, X} from '@steeze-ui/heroicons'
 import { Menu as MenuIcon } from '@steeze-ui/heroicons'
-import { Cog } from '@steeze-ui/heroicons'
 export let basepath: string = '';
 
 const logout = async () => {
-  await loki.user.logout();
+  await loki.user?.logout() || console.warn('loki.user unavailable; logout message received.')
   document.location.reload();
 };
 export let navItems: ({ name: string; icon: { default: IconThemeSource } & { [p: string]: IconThemeSource }; href: string })[];
@@ -62,14 +61,16 @@ onMount(() => url = window.location.href);
           <img class="hidden lg:block h-8 w-auto" src="https://saplingdata.com/wp-content/themes/sapling/img/sapling-data-logo.svg" alt="Sapling Data" />
         </div>
         <div class="hidden sm:block sm:ml-6">
-          <div class="flex items-center space-x-4">
+          <div class="flex items-center space-x-2">
             <Router>
               {#each navItems as item}
                 <Link
                         to={basepath + item.href}
                         class="inline-block text-white flex items-center px-2 py-1.5 text-xxs rounded-md"
                 >
-                  <Icon src={item.icon} theme="outline" class="text-blue-500 block h-4 w-4 mr-2" />
+                  {#if item.icon}
+                    <Icon src={item.icon} theme="outline" class="text-blue-500 block h-4 w-4 mr-2" />
+                  {/if}
                   {item.name}
                 </Link>
               {/each}
@@ -77,11 +78,11 @@ onMount(() => url = window.location.href);
           </div>
         </div>
       </div>
-      <div class="absolute inset-y-0 right-0 z-50 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+      <div class="absolute inset-y-0 right-0 z-50 flex items-center pr-2 sm:static sm:inset-auto sm:pr-0">
         <Menu as="div" class="relative inline-block text-left">
           <div>
             <MenuButton class="group w-full bg-gray-800 px-2 py-2 text-sm text-left font-medium">
-              <span class="flex w-full justify-between items-center">
+              <span class="flex w-full justify-between items-center space-x-2">
                 <span class="flex min-w-0 items-center justify-between space-x-2">
                   <div class="w-8 h-8 bg-blue-500 group-hover:bg-blue-600 flex flex-col rounded-full items-center">
                     <span class="my-auto text-sm font-semibold text-white">
@@ -97,7 +98,7 @@ onMount(() => url = window.location.href);
             </MenuButton>
           </div>
           <Transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
-            <MenuItems class="z-10 mx-3 p-1.5 origin-top absolute right-0 left-0 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-200 focus:outline-none">
+            <MenuItems class="z-10 mx-3 p-1.5 origin-top absolute right-0 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-200 focus:outline-none">
               <div>
                 <MenuItem class="flex flex-row">
                   <button
@@ -119,6 +120,7 @@ onMount(() => url = window.location.href);
                 </MenuItem>
                 <MenuItem class="flex flex-row">
                   <button
+                          on:click={logout}
                           class="text-white flex w-full items-center px-2 py-1.5
                   text-xxs hover:bg-gray-200 rounded-md
                   text-gray-700 whitespace-nowrap">
