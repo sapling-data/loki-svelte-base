f378a3c9c1e7c0e4cc776fe54d062d57788d2700
Andrew Waller <andrew.waller@saplingdata.com>
2022-07-14 20:59:35 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,31 @@
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import injectLokiFreemarkerPlugin from './plugins/injectLokiFreemarker';
+import injectDevMainPlugin from './plugins/injectDevMain';
+import lokiConfig from './loki.config.mjs';
 import postcss from './postcss.config.cjs';
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
+const { resolve } = require('path');
+
 export default defineConfig({
-  plugins: [svelte()],
-  css:{
-    postcss
-  }
-})
+  root: './',
+  base: process.env.NODE_ENV === 'production' ? `/${lokiConfig.appName}/api/urn/com/loki/core/model/api/modelResource/v/urn/com/${lokiConfig.cloudName}/${lokiConfig.appName}/app/pages/${lokiConfig.pageName}/` : '/',
+  plugins: [
+    svelte(),
+    injectDevMainPlugin(),
+    injectLokiFreemarkerPlugin(),
+  ],
+  css: {
+    postcss,
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        html: resolve(__dirname, 'index.html'),
+        main: resolve(__dirname, 'src/main.ts'),
+      },
+    },
+    outDir: 'dist',
+    assetsDir: './',
+  },
+});
