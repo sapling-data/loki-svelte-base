79d9dd8af83bf216debb1956f2a485241f22956b
Andrew Waller <andrew.waller@saplingdata.com>
2022-07-19 09:51:24 -0500

    Add title to Notification type/methods; add semicolons

--- src/lib/notifications.ts
+++ src/lib/notifications.ts
@@ -1,59 +1,59 @@
-import { writable, derived } from "svelte/store"
-import type { Writable } from "svelte/store"
+import { writable, derived } from 'svelte/store';
+import type { Writable } from 'svelte/store';
 
-const DEFAULT_DURATION = 5000
+const DEFAULT_DURATION = 3000;
 
 type Notification = {
   id?: string,
   type?: string,
+  title: string,
   message: string,
   duration: number,
   dismissable: boolean,
 }
 
 function createNotificationStore(duration: number = DEFAULT_DURATION) {
-  const _notifications: Writable<Notification[]> = writable([])
+  const _notifications: Writable<Notification[]> = writable([]);
 
   function send(notification: Notification) {
     notification.id = getId();
     _notifications.update(state => {
-      return [...state, notification]
-    })
+      return [...state, notification];
+    });
   }
 
   function dismiss(id: string) {
     _notifications.update(state => {
       return [...state.filter(notification => notification.id !== id)];
-    })
+    });
   }
 
-  let timers = []
-
   const notifications = derived(_notifications, ($_notifications: Notification[], set) => {
-    set($_notifications)
+    set($_notifications);
     if ($_notifications.length > 0) {
       const timer = setTimeout(() => {
         _notifications.update(state => {
-          state.shift()
-          return state
-        })
-      }, $_notifications[0].duration)
+          state.shift();
+          return state;
+        });
+      }, $_notifications[0].duration);
       return () => {
-        clearTimeout(timer)
+        clearTimeout(timer);
       }
     }
-  })
-  const { subscribe } = notifications
+  });
+
+  const { subscribe } = notifications;
 
   return {
     subscribe,
     send,
     dismiss,
-    default: (message: string, duration: number = DEFAULT_DURATION, dismissable: boolean = true) => send({message, type: 'default', duration, dismissable}),
-    error: (message: string, duration: number = DEFAULT_DURATION, dismissable: boolean = true) => send({message, type: 'error', duration, dismissable}),
-    warning: (message: string, duration: number = DEFAULT_DURATION, dismissable: boolean = true) => send({message, type: 'warning', duration, dismissable}),
-    info: (message: string, duration: number = DEFAULT_DURATION, dismissable: boolean = true) => send({message, type: 'info', duration, dismissable}),
-  }
+    default: (message: string, title: string, duration: number = DEFAULT_DURATION, dismissable: boolean = true) => send({title, message, type: 'default', duration, dismissable}),
+    error: (message: string, title: string = 'Error', duration: number = DEFAULT_DURATION, dismissable: boolean = true) => send({title, message, type: 'error', duration, dismissable}),
+    warning: (message: string, title: string = 'Warning', duration: number = DEFAULT_DURATION, dismissable: boolean = true) => send({title, message, type: 'warning', duration, dismissable}),
+    info: (message: string, title: string = 'Information', duration: number = DEFAULT_DURATION, dismissable: boolean = true) => send({title, message, type: 'info', duration, dismissable}),
+  };
 }
 
 function getId() {
