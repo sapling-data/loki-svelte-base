5156ba98444ee748cde07c01f51d2b158447eb62
Andrew Waller <andrew.waller@saplingdata.com>
2022-07-19 09:47:16 -0500

    Add gap for toasts

--- src/lib/components/Notifications.svelte
+++ src/lib/components/Notifications.svelte
@@ -3,9 +3,10 @@
   import Toast from "./Toast.svelte";
 </script>
 
-<div class="notifications">
+<div class="notifications gap-3">
   {#each $notifications as notification (notification.id)}
     <Toast
+            title={notification.title}
             type={notification.type}
             message={notification.message}
             id={notification.id}
