b8bbb2562e35f6734bf9e2b5321143528c2feada
Andrew Waller <andrew.waller@saplingdata.com>
2022-07-21 11:53:17 -0500

    Add ErrorIndicator.svelte

---
+++ src/lib/components/ErrorIndicator.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import {Icon} from "@steeze-ui/svelte-icon";
+  import {Exclamation, XCircle} from "@steeze-ui/heroicons";
+
+  export let message: string;
+</script>
+
+<span class="inline-flex items-center px-2.5 py-0.5 text-xs font-medium text-red-700">
+  <Icon src={Exclamation} theme="solid" class="-ml-1 mr-2 h-4 w-4 text-red-500" />
+  <span>{message}</span>
+</span>
