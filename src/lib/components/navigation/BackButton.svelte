46c8fe26f6f9a00fae49d1a8607e1ccdd67673c9
Andrew Waller <andrew.waller@saplingdata.com>
2022-07-21 11:52:43 -0500

    Add basic back button

---
+++ src/lib/components/navigation/BackButton.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import {Icon} from "@steeze-ui/svelte-icon";
+  import {ArrowLeft} from "@steeze-ui/heroicons";
+
+  function goBack() {
+    window.history.back();
+  }
+</script>
+
+<button
+        on:click={goBack}
+        type="button"
+        class="inline-flex items-center -ml-2.5 px-2.5 py-1.5 text-xs rounded text-gray-700 hover:bg-blue-100 hover:text-blue-500 focus:outline-none">
+        <Icon src={ArrowLeft} theme="solid" class="h-4 w-4 mr-2" />
+        <span class="my-auto">Back</span>
+</button>
