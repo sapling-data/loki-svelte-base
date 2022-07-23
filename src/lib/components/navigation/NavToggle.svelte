c048bbe8929942c2f0b1234c52492ff39feadc48
Andrew Waller <andrew.waller@saplingdata.com>
2022-07-23 15:28:17 -0500

    Update colors for dark mode and update spacing for icons

--- src/lib/components/navigation/NavToggle.svelte
+++ src/lib/components/navigation/NavToggle.svelte
@@ -11,19 +11,19 @@
   <button
           on:click={() => minimal = !minimal}
           type="button"
-          class="my-auto text-gray-400 flex items-center px-2 py-1.5 text-xxs
-                      rounded-md hover:bg-gray-700 hover:text-white gap-1"
+          class="my-auto text-gray-600 hover:text-gray-600 hover:bg-gray-200 dark:text-gray-400 flex items-center px-2 py-1.5 text-xxs
+                      rounded-md dark:hover:bg-gray-800 dark:hover:text-white gap-1"
   >
-    <Icon src={minimal ? SidebarExpand : SidebarCollapse}  class="{ minimal ? 'h-4 w-4' : 'h-3 w-3' } rotate-180" />
-    <span class="{ minimal && 'hidden' } my-auto">Minimize sidebar</span>
+    <Icon src={minimal ? SidebarExpand : SidebarCollapse}  class=" h-3 w-3 { minimal && 'ml-0.5 my-0.5' } rotate-180" />
+    <span class="{ minimal && 'hidden' } ml-2 my-auto">Minimize sidebar</span>
   </button>
   <button
           on:click={() => show = !show}
           type="button"
-          class="my-auto text-gray-400 flex items-center px-2 py-1.5 text-xxs
-                      rounded-md hover:bg-gray-700 hover:text-white gap-2"
+          class="my-auto text-gray-600 hover:text-gray-600 hover:bg-gray-200 dark:text-gray-400 flex items-center px-2 py-1.5 text-xxs
+                      rounded-md dark:hover:bg-gray-800 dark:hover:text-white gap-2"
   >
-    <Icon src={X} theme="solid" class="{ minimal ? 'h-4 w-4' : 'h-3 w-3' }" />
-    <span class="{ minimal && 'hidden' } my-auto">Close sidebar</span>
+    <Icon src={X} theme="solid" class="h-3 w-3 { minimal && 'ml-0.5 my-0.5' }" />
+    <span class="{ minimal && 'hidden' } ml-1 my-auto">Close sidebar</span>
   </button>
 </div>
