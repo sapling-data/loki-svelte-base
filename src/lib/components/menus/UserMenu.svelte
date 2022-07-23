c7dac6993c304554d0b31dae2fc97eea6c6ecd84
Andrew Waller <andrew.waller@saplingdata.com>
2022-07-23 14:06:39 -0500

    Update styling/config options

--- src/lib/components/menus/UserMenu.svelte
+++ src/lib/components/menus/UserMenu.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
-  import { fade } from 'svelte/transition';
-  import {Menu, MenuButton, MenuItem, MenuItems} from "@rgossiaux/svelte-headlessui";
-  import {Briefcase, ChevronDown, ChevronUp, Logout, User} from "@steeze-ui/heroicons";
+  import {
+    Popover,
+    PopoverButton,
+    PopoverPanel
+  } from "@rgossiaux/svelte-headlessui";
+  import { createPopperActions } from 'svelte-popperjs';
+  import {Briefcase, ChevronDown, Logout, User} from "@steeze-ui/heroicons";
   import {Icon} from "@steeze-ui/svelte-icon";
+  const [popperRef] = createPopperActions();
   export let navMode: string;
+  export let minimal: boolean;
   let userData = loki.user?.getUserData() || {
     urn: 'urn:com:saplingdata:domain:security:users:testUser',
     name: 'Test User'
@@ -18,23 +24,44 @@
     }
     return initials
   }
+  const menuItems = [
+    {
+      title: 'Profile',
+      icon: User,
+      href: '/'
+    },
+    {
+      title: 'Profile',
+      icon: Briefcase,
+      href: '/'
+    },
+    {
+      title: 'Logout',
+      icon: Logout,
+      href: '/'
+    }
+  ]
 </script>
 
-<Menu as="div" class="mt-auto relative inline-block text-left" let:open>
-  <div>
-    <MenuButton class="group w-full bg-gray-800 px-2 py-2 text-sm text-left font-medium">
-      <div class="flex w-full justify-between items-center">
-        <div class="flex min-w-0 items-center justify-between space-x-2">
+<Popover class="{!minimal && 'px-2'} py-2" let:open>
+  <PopoverButton
+          use={[popperRef]}
+          class="group w-full bg-gray-800 text-sm text-left font-medium"
+  >
+    <div class="flex w-full justify-between items-center">
+      <div class="flex min-w-0 items-center {!minimal && 'justify-between space-x-2'}">
+        <div class="flex-1 flex flex-col min-w-0">
           <div
-                  class="w-8 h-8 bg-blue-500 group-hover:bg-blue-600 flex flex-col rounded-full items-center"
+                  class="{minimal && 'mx-auto'} w-8 h-8 bg-blue-500 group-hover:bg-blue-600 flex flex-col rounded-full items-center"
                   class:bg-blue-600={open}
                   class:bg-blue-500={!open}
           >
-            <span class="my-auto text-sm font-semibold text-white">
-              {getInitials(userData.name)}
-            </span>
+              <span class="my-auto text-sm font-semibold text-white">
+                {getInitials(userData.name)}
+              </span>
           </div>
-          <span class="flex-1 flex flex-col min-w-0">
+        </div>
+        {#if !minimal}
             <span
                     class="text-xxs font-medium truncate group-hover:text-blue-300"
                     class:text-blue-300={open}
@@ -42,67 +69,52 @@
             >
               {userData.name}
             </span>
-          </span>
-        </div>
+        {/if}
+      </div>
+      {#if !minimal}
         <div
                 class="group-hover:text-blue-300"
                 class:text-blue-300={open}
                 class:text-gray-400={!open}
         >
-          <Icon src={navMode === 'sidebar' ? ChevronUp : ChevronDown} class="ml-4 flex-shrink-0 h-4 w-4" aria-hidden="true" />
+          <Icon
+                  src={ChevronDown}
+                  class="flex-shrink-0 h-4 w-4 ml-2"
+                  aria-hidden="true" />
         </div>
-      </div>
-    </MenuButton>
-  </div>
+      {/if}
+    </div>
+  </PopoverButton>
   {#if open}
-    <div out:fade="{{ duration: 125 }}">
-      <MenuItems
-              class="z-10 p-1.5 transform absolute right-0 left-0 bg-white rounded-md
-              shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none
-              {navMode === 'sidebar' ? 'origin-top-right -translate-y-full -top-2' : 'mt-[-0.25rem]'}"
+    <div
+            class:z-50={open}
+    >
+      <PopoverPanel
+              class="menu-container p-1.5 absolute max-h-96 w-48 overflow-y-auto
+                origin-bottom-left bg-white rounded-md shadow-lg ring-1 ring-black
+                ring-opacity-5 focus:outline-none {navMode === 'sidebar' ? '-translate-y-full -bottom-12' : 'top-11 right-1'}"
       >
-        <div>
-          <MenuItem class="flex flex-row">
-            <button
-                    class="text-white flex w-full items-center px-2 py-1.5
+        {#each menuItems as item }
+          <button
+                  href={item.link}
+                  class="flex flex-row w-full items-center px-2 py-1.5
                     text-xxs hover:bg-gray-200 rounded-md
                     text-gray-700 whitespace-nowrap">
-              <Icon
-                      src={User}
-                      theme="outline"
-                      class="block h-4 w-4 mr-2"
-              />
-              <span class="my-auto text-xxs">Profile</span>
-            </button>
-          </MenuItem>
-          <MenuItem class="flex flex-row">
-            <button
-                    class="text-white flex w-full items-center px-2 py-1.5
-                    text-xxs hover:bg-gray-200 rounded-md
-                    text-gray-700 whitespace-nowrap">
-              <Icon
-                      src={Briefcase}
-                      theme="outline"
-                      class="block h-4 w-4 mr-2"
-              />
-              <span class="my-auto text-xxs">Account</span>
-            </button>
-          </MenuItem>
-          <MenuItem class="flex flex-row">
-            <button
-                    class="text-white flex w-full items-center px-2 py-1.5
-                    text-xxs hover:bg-gray-200 rounded-md
-                    text-gray-700 whitespace-nowrap">
-              <Icon
-                      src={Logout}
-                      theme="outline"
-                      class="block h-4 w-4 mr-2"
-              />
-              <span class="my-auto text-xxs">Log out</span>
-            </button>
-          </MenuItem>
-        </div>
-      </MenuItems>
+            <Icon
+                    src={item.icon}
+                    theme="outline"
+                    class="block h-4 w-4 mr-2 text-blue-500"
+            />
+            <span class="my-auto text-xxs">{ item.title }</span>
+          </button>
+        {/each}
+      </PopoverPanel>
     </div>
   {/if}
-</Menu>
+</Popover>
+
+<style>
+    div :global(.menu-container) {
+        z-index: 9000 !important;
+    }
+</style>
