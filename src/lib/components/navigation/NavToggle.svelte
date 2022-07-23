769b82d7eef23dfa8da5fcca7cc7e0d34bcff686
Andrew Waller <andrew.waller@saplingdata.com>
2022-07-23 14:07:31 -0500

    Add NavToggle.svelte

---
+++ src/lib/components/navigation/NavToggle.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import {Icon} from "@steeze-ui/svelte-icon";
+  import {X} from "@steeze-ui/heroicons";
+  import {SidebarCollapse, SidebarExpand} from "@steeze-ui/octicons";
+
+  export let show: boolean;
+  export let minimal: boolean;
+</script>
+
+<div class="flex flex-col gap-2">
+  <button
+          on:click={() => minimal = !minimal}
+          type="button"
+          class="my-auto text-gray-400 flex items-center px-2 py-1.5 text-xxs
+                      rounded-md hover:bg-gray-700 hover:text-white gap-1"
+  >
+    <Icon src={minimal ? SidebarExpand : SidebarCollapse}  class="{ minimal ? 'h-4 w-4' : 'h-3 w-3' } rotate-180" />
+    <span class="{ minimal && 'hidden' } my-auto">Minimize sidebar</span>
+  </button>
+  <button
+          on:click={() => show = !show}
+          type="button"
+          class="my-auto text-gray-400 flex items-center px-2 py-1.5 text-xxs
+                      rounded-md hover:bg-gray-700 hover:text-white gap-2"
+  >
+    <Icon src={X} theme="solid" class="{ minimal ? 'h-4 w-4' : 'h-3 w-3' }" />
+    <span class="{ minimal && 'hidden' } my-auto">Close sidebar</span>
+  </button>
+</div>
