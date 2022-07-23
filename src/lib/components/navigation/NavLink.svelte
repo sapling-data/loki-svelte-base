19f9b1c1da2f7f126c6db4ae038c42aad74f871c
Andrew Waller <andrew.waller@saplingdata.com>
2022-07-23 14:07:06 -0500

    Update styling/config options

--- src/lib/components/navigation/NavLink.svelte
+++ src/lib/components/navigation/NavLink.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
-import type {NavItem} from "../../types";
-import { Link } from 'svelte-routing';
-import { Icon } from '@steeze-ui/svelte-icon';
-export let to: string;
-export let icon: NavItem;
-export let name: string;
+  import type {NavItem} from "../../types";
+  import { Link } from 'svelte-routing';
+  import { Icon } from '@steeze-ui/svelte-icon';
+  export let to: string;
+  export let icon: NavItem;
+  export let name: string;
+  export let minimal: boolean;
 </script>
 
 <Link
@@ -12,7 +13,7 @@ export let name: string;
         class="my-auto text-white flex items-center px-2 py-1.5 text-xxs rounded-md"
 >
   {#if icon}
-    <Icon src={icon} theme="outline" class="text-blue-500 block h-4 w-4 mr-2" />
+    <Icon src={icon} theme="outline" class="text-blue-500 block h-4 w-4" />
   {/if}
-  {name}
+  <span class="{ minimal && 'hidden' } ml-2 whitespace-nowrap">{name}</span>
 </Link>
