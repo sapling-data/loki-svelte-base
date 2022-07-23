e6b7509622b0b077295154ce18b5a3d8edd46257
Andrew Waller <andrew.waller@saplingdata.com>
2022-07-23 18:16:51 -0500

    Update dark mode colors

--- src/lib/components/ErrorIndicator.svelte
+++ src/lib/components/ErrorIndicator.svelte
@@ -5,7 +5,7 @@
   export let message: string;
 </script>
 
-<span class="inline-flex items-center px-2.5 py-0.5 text-xs font-medium text-red-700">
-  <Icon src={Exclamation} theme="solid" class="-ml-1 mr-2 h-4 w-4 text-red-500" />
+<span class="inline-flex items-center px-2.5 py-0.5 text-xs font-medium dark:text-red-400 text-red-700">
+  <Icon src={Exclamation} theme="solid" class="-ml-1 mr-2 h-4 w-4 dark:text-red-400 text-red-500" />
   <span>{message}</span>
 </span>
