c7f1acfb69c67f27fc443ed6f3c6d763c6475e7f
Andrew Waller <andrew.waller@saplingdata.com>
2022-07-23 18:15:12 -0500

    Update dark mode colors

--- src/lib/components/navigation/BackButton.svelte
+++ src/lib/components/navigation/BackButton.svelte
@@ -10,7 +10,9 @@
 <button
         on:click={goBack}
         type="button"
-        class="inline-flex items-center -ml-2.5 px-2.5 py-1.5 text-xs rounded text-gray-700 hover:bg-blue-100 hover:text-blue-500 focus:outline-none">
+        class="inline-flex items-center -ml-2.5 px-2.5 py-1.5 text-xs rounded dark:text-gray-400
+        dark:hover:bg-gray-800 dark:hover:text-white text-gray-700 hover:bg-blue-100
+        hover:text-blue-500 focus:outline-none">
         <Icon src={ArrowLeft} theme="solid" class="h-4 w-4 mr-2" />
         <span class="my-auto">Back</span>
 </button>
