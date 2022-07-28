e726b343e5a6298cdd64d67a6cdba262dab4b9da
Andrew Waller <andrew.waller@saplingdata.com>
2022-07-28 16:24:25 -0500

    Update conditional styling for thinNavbar mode

--- src/lib/components/navigation/NavLink.svelte
+++ src/lib/components/navigation/NavLink.svelte
@@ -12,7 +12,7 @@
 
 <Link
         to={to}
-        class="my-auto rounded-md text-gray-600 dark:text-white flex items-center px-2 py-1.5 text-xxs"
+        class="my-auto {navMode === 'navbar' && thinNavbar ? '' : 'rounded-md'} text-gray-600 dark:text-white flex items-center px-2 py-1.5 text-xxs"
 >
   {#if icon}
     <Icon src={icon} theme="outline" class="text-blue-500 block h-4 w-4" />
