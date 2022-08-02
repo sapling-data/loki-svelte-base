68db9e0a87bf546fbd98e9f1267f081f82795282
Andrew Waller <andrew.waller@saplingdata.com>
2022-08-02 14:13:14 -0500

    Remove tsignore comments; add whitespace; add AppConfig type; import NavItem type

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,13 +1,28 @@
 import lokiConfig from '../../loki.config.mjs';
-// @ts-ignore
 import Welcome from './pages/Welcome.svelte';
-import {BookOpen, Home} from '@steeze-ui/heroicons';
-// @ts-ignore
+import { BookOpen, Home } from '@steeze-ui/heroicons';
 import Index from './pages/Index.svelte';
-import type { AppConfig } from './types';
+import type { NavItem } from './types';
 
-// @ts-ignore
-const basePath = import.meta.env.MODE === 'development' ? `/${lokiConfig.appName}/pages/urn/com/${lokiConfig.cloudName}/${lokiConfig.appModelName}/app/pages/${lokiConfig.pageName}/v` : `/${loki.urn.getLastSegment(loki.app.appInstanceUrn)}/pages/urn/com/${lokiConfig.appRoot}/${lokiConfig.appModelName}/app/pages/${lokiConfig.pageName}/v`;
+const basePath: string =
+  import.meta.env.MODE === 'development'
+    ? `/${lokiConfig.appName}/pages/urn/com/${lokiConfig.cloudName}/${lokiConfig.appModelName}/app/pages/${lokiConfig.pageName}/v`
+    : `/${loki.urn.getLastSegment(loki.app.appInstanceUrn)}/pages/urn/com/${
+        lokiConfig.appRoot
+      }/${lokiConfig.appModelName}/app/pages/${lokiConfig.pageName}/v`;
+
+interface AppConfig {
+  title: string;
+  basePath: string;
+  navMode: string;
+  navItems: NavItem[];
+  allowNavToggle: boolean;
+  displayLogo: boolean;
+  displayCloudMenu: boolean;
+  displayUserMenu: boolean;
+  thinNavbar: boolean;
+  darkByDefault: boolean;
+}
 
 export const appConfig: AppConfig = {
   basePath,
@@ -31,8 +46,7 @@ export const appConfig: AppConfig = {
       name: 'Index',
       to: `${basePath}/workflows`,
       component: Index,
-      icon: BookOpen
-    }
+      icon: BookOpen,
+    },
   ],
-}
-
+};
