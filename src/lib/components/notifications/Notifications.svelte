b4221c8c435ccc5fe1c77d8ffbdb8a0a7d89b76e
Andrew Waller <andrew.waller@saplingdata.com>
2022-08-02 14:02:52 -0500

    Add notifications component

---
+++ src/lib/components/notifications/Notifications.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { notifications } from '../../notifications';
+  import Toast from "./Toast.svelte";
+</script>
+
+<div class="notifications gap-3">
+  {#each $notifications as notification (notification.id)}
+    <Toast { ...notification } />
+  {/each}
+</div>
