30a153e8af8c7be971a160d47f1a100fa4af38f7
Andrew Waller <andrew.waller@saplingdata.com>
2022-10-07 10:53:21 -0500

    Add allowClose option

--- src/lib/components/navigation/NavToggle.svelte
+++ src/lib/components/navigation/NavToggle.svelte
@@ -5,6 +5,7 @@
 
   export let show: boolean;
   export let minimal: boolean;
+  export let allowClose: boolean;
 </script>
 
 <div class="flex flex-col gap-2">
@@ -17,13 +18,15 @@
     <Icon src={minimal ? SidebarExpand : SidebarCollapse}  class=" h-3 w-3 { minimal && 'ml-0.5 my-0.5' } rotate-180" />
     <span class="{ minimal && 'hidden' } ml-2 my-auto">Minimize sidebar</span>
   </button>
-  <button
-          on:click={() => show = !show}
-          type="button"
-          class="my-auto text-gray-600 hover:text-gray-600 hover:bg-gray-200 dark:text-gray-400 flex items-center px-2 py-1.5 text-xxs
-                      rounded-md dark:hover:bg-gray-800 dark:hover:text-white gap-2"
-  >
-    <Icon src={X} theme="solid" class="h-3 w-3 { minimal && 'ml-0.5 my-0.5' }" />
-    <span class="{ minimal && 'hidden' } ml-1 my-auto">Close sidebar</span>
-  </button>
+  {#if allowClose}
+    <button
+            on:click={() => show = !show}
+            type="button"
+            class="my-auto text-gray-600 hover:text-gray-600 hover:bg-gray-200 dark:text-gray-400 flex items-center px-2 py-1.5 text-xxs
+                        rounded-md dark:hover:bg-gray-800 dark:hover:text-white gap-2"
+    >
+      <Icon src={X} theme="solid" class="h-3 w-3 { minimal && 'ml-0.5 my-0.5' }" />
+      <span class="{ minimal && 'hidden' } ml-1 my-auto">Close sidebar</span>
+    </button>
+  {/if}
 </div>
