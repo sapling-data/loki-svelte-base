c7930f112ab9c06bc287cd8aafdf1e1f8c846b02
Andrew Waller <andrew.waller@saplingdata.com>
2022-10-07 10:54:16 -0500

    Add AccessDenied.svelte and ProtectedRoute.svelte

---
+++ src/lib/components/navigation/ProtectedRoute.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { Route } from 'svelte-routing';
+  import AccessDenied from "./AccessDenied.svelte";
+
+  export let path;
+  export let component;
+  export let guard;
+
+  $: isAuthorized = guard();
+</script>
+
+{#if isAuthorized}
+  <Route path={path} component={component} />
+{:else}
+  <Route path={path} component={AccessDenied} />
+{/if}
